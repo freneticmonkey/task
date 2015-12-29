1a279a63fe4d00fd6f2b03fa1d037b5951b201ac
Porter, Scott <sporter@ea.com>
2015-12-30 00:05:21 +1100

    Initial Commit

---
+++ task/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  task
+//
+//  Created by Porter, Scott on 30/12/2015.
+//  Copyright © 2015 Porter, Scott. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
